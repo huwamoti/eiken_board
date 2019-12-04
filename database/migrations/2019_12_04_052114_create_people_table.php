50a0518a097975166fac9cd72eadbb45ace13c27
amenbo <a_moonlightstationu@yahoo.co.jp>
2019-12-04 14:28:06 +0900

    テーブル生成の処理

---
+++ database/migrations/2019_12_04_052114_create_people_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eiken', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('category');
+            $table->string('message');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eiken');
+    }
+}
