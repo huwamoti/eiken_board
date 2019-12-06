c6f71acde8792f8378455dc5b613c2ccee7b3fdc
amenbo <a_moonlightstationu@yahoo.co.jp>
2019-12-06 15:14:59 +0900

    ログインしたら掲示板のトップページに遷移するようにしました。

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/eiken';
 
     /**
      * Create a new controller instance.
