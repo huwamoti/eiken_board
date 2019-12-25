99c392683f00740f75334eeebf815a6c29812175
amenbo <a_moonlightstationu@yahoo.co.jp>
2019-12-25 23:00:26 +0900

    eikenテーブルをuser_idと紐づけして、ユーザーは自分の投稿だけを消去できるようにしました。

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+
 class HomeController extends Controller
 {
     /**
@@ -31,13 +32,14 @@ class HomeController extends Controller
 
     public function create(Request $request)
     {
-        
+        $user=\Auth::user();
         $param=[
             'name'=>$request->name,
             'category'=>$request->category,
             'message'=>$request->message,
+            'user_id'=>$user->id,
         ];
-        DB::insert('insert into eiken (name,category,message) values (:name,:category,:message)',$param);
+        DB::insert('insert into eiken (name,category,message,user_id) values (:name,:category,:message,:user_id)',$param);
         return redirect('/home');
        
     }
@@ -45,9 +47,9 @@ class HomeController extends Controller
    
     public function remove(Request $request)
     {
-        
-            $param=['id'=>$request->id];
-            DB::delete('delete from eiken where id=:id',$param);
+            $user=\Auth::user();
+            $param=['user_id'=>$user->id];
+            DB::delete('delete from eiken where user_id=:user_id',$param);
             return redirect('/home');
         
        
